60e154202d68e1782a8f04010816cdf355c6f066
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-10-28 14:34:22 +0300

    part 1 chapter 1-3 finished

---
+++ part1/2-3-Name-and-address.py
@@ -0,0 +1,8 @@
+givenName = input("Given name: ")
+familyName = input("Family name: ")
+address = input("Street address: ")
+city = input("City and postal code: ")
+
+print(givenName + " " + familyName)
+print(address)
+print(city)
