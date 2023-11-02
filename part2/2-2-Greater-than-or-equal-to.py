0438195bb71297951cfab3b69ee95cfb7a4c080c
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-11-02 20:25:10 +0200

    part2 chapter2 finished

---
+++ part2/2-2-Greater-than-or-equal-to.py
@@ -0,0 +1,9 @@
+num1 = int(input("Please type in the first number: "))
+num2 = int(input("Please type in the second number: "))
+
+if num1 > num2:
+    print(f"The greater number was: {num1}")
+elif num1 == num2:
+    print("The numbers are equal!")
+elif num1 < num2:
+    print(f"The greater number was: {num2}")
